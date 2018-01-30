90d65950fc8784063352fd0d541f7357817d4c02
Cameron Deardorff <camdeardorff@camdeardorff.com>
2018-01-30 15:46:12 -0500

    added handlers from alexa sdk example

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const Alexa = require('alexa-sdk');
+const constants = require("./constants.json");
 
+console.log("hello world");
+
+
+// define handlers for handling requests.
+// These will be split out into separate files moving forward
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('HelloWorldIntent');
+    },
+
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
+
+
+// setup the alexa sdk in here.
 exports.handler = function(event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
-    alexa.appId = APP_ID // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+    alexa.appId = constants.APP_ID; // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+    alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
